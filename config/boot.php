50d61572c8747ed3c3f317f4228b8b2abb34d129
querwurzelt <>
2019-01-23 00:05:18 +0100

    support PHP 7.3, MySQl 5.7+ - railsphp package pointed to bugfix github repo with dependency versions updated

--- config/boot.php
+++ config/boot.php
@@ -14,7 +14,7 @@ define('RAILS_ROOT', dirname(__DIR__));
 /**
  * Path to Rails.
  */
-$railsPath = __DIR__ . '/../vendor/railsphp/railsphp/lib/Rails';
+$railsPath = dirname(__DIR__, 1) . '/vendor/railsphp/railsphp/lib/Rails';
 
 /**
  * Load and initialize Rails.
